43e51af310add708fb8c08d53c006c4aa6fb5180
Pichaya Viwatassawin <pichaya.viwatassawin@student.uts.edu.au>
2026-09-04 22:57:07 +1000

    Initial Commit

---
+++ VisaMatch/VisaMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisaMatchApp.swift
+//  VisaMatch
+//
+//  Created by Pichaya Viwatassawin on 4/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct VisaMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
